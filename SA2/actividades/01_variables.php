8bd37bfe6a7a80ca21236979187357cb0a88c282
conbega <c.benaventgavidia@edu.gva.es>
2025-09-30 16:40:44 +0200

    cambio estructura

---
+++ SA2/actividades/01_variables.php
@@ -0,0 +1,16 @@
+<?php
+/* Crea un script en PHP que declare tres variables:
+Una variable $nombre con tu nombre.
+Una variable $edad con tu edad.
+Una variable $edadEn10 que almacene la edad que tendrás dentro de 10 años.
+Muestra el mensaje:
+
+Hola, me llamo [nombre], tengo [edad] años y dentro de 10 años tendré [edadEn10].
+
+*/
+
+$nombre = "Ana";
+$edad = 20;
+$edadEn10 = $edad + 10;
+echo "Hola, me llamo $nombre, tengo $edad años y dentro de 10 años tendré $edadEn10.";
+?>
---
+++ SA2/actividades/comentarios.php
@@ -0,0 +1,21 @@
+<?php
+/* Corrige el siguiente código añadiendo comentarios en las líneas correctas para que sea fácil de entender. 
+
+
+<?php
+$a = 10;
+$b = 2;
+$c = $a / $b;
+echo $c;
+?>
+*/
+
+# Solución (con comentarios en distintos formatos):
+
+$a = 10; // Variable con valor 10
+$b = 2; # Variable con valor 2
+/* Dividimos $a entre $b
+y lo guardamos en $c */
+$c = $a / $b;
+echo $c; // Imprime 5
+?>
